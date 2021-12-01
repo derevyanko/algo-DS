b5d226d5d4d5d3368ef3c70fceeb63f4529c47d5
Oleg Derevyanko <trekol39042@gmail.com>
2021-12-01 13:01:15 +0200

    #1; Remove functions header .

--- graph/functions.h
+++
@@ -1,9 +0,0 @@
-InputData readInputData(istream&);
-
-int64_t FordBelmanAlgorithm(InputData const&, const int);
-
-AnswerType calculateMinimumWaysSum(InputData const&);
-
-void printMinimumSumAnswer(ostream&, const AnswerType);
-
-#include "functions.cpp"
--- graph/main.h
+++ graph/main.h
@@ -15,4 +15,13 @@ struct AnswerType {
 };
 
 #include "InputData.h"
-#include "functions.h"
+
+InputData readInputData(istream&);
+
+int64_t FordBelmanAlgorithm(InputData const&, const int);
+
+AnswerType calculateMinimumWaysSum(InputData const&);
+
+void printMinimumSumAnswer(ostream&, const AnswerType);
+
+#include "functions.cpp"
